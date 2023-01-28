3d2af85f351dc1253ff726ae219d2eabb940bf82
NangHsu04 <byunbaek2181@gmail.com>
2023-01-28 21:40:12 +0630

    First Commit

---
+++ JavaBasic/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        int a= 61;
+
+        if (a < 40) {
+            System.out.println("Fail");
+        }
+        else if(a>=40){
+            System.out.println("Pass");
+        }
+    }
+
+}
